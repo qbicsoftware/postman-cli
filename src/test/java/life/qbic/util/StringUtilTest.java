4f2147c1b9ecd65eac3c029c942a6c6fe3dba8f2
Sven F <sven.fillinger@mailbox.org>
2020-06-25 15:22:32 +0200

    Update test method
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

--- src/test/java/life/qbic/util/StringUtilTest.java
+++ src/test/java/life/qbic/util/StringUtilTest.java
@@ -8,6 +8,11 @@ public class StringUtilTest {
 
   @Test
   public void endsWithIgnoreCase() {
+    // success with same case
+    assertEquals(true, StringUtil.endsWithIgnoreCase("thisissomerandomTESTBLA", "randomTESTBLA"));
+    assertEquals(true, StringUtil.endsWithIgnoreCase("thisissomerandomTESTBLA", "randomTESTBlA"));
+     assertEquals(true, StringUtil.endsWithIgnoreCase("thisissomerandomTESTBLA", "randomteSTBlA"));
+    // success with different case
     assertEquals(true, StringUtil.endsWithIgnoreCase("thisissomerandomTESTBLA", "randomTESTbla"));
     assertEquals(false, StringUtil.endsWithIgnoreCase("thisissomerandomTESTBLA", "ayyyyynope"));
   }
