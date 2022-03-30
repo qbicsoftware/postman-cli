5ccc3d27e279d66bfed338bf8a05006cf4948804
Steffengreiner <Steffen.Greiner@gmx.de>
2022-03-30 16:09:47 +0200

    specify path of logfile for invalid checksums in warning message

--- src/main/java/life/qbic/model/download/QbicDataDownloader.java
+++ src/main/java/life/qbic/model/download/QbicDataDownloader.java
@@ -431,7 +431,7 @@ public class QbicDataDownloader {
 
   public void notifyUserOfInvalidChecksum() {
     if (invalidChecksumOccurred) {
-      LOG.warn("Checksum mismatches were detected during file download, check the log file for details");
+      LOG.warn("Checksum mismatches were detected during file download, check the logs/summary_invalid_files.txt log file for details");
     }
   }
 
