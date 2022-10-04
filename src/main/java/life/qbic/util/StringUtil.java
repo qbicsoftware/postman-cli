f862ec691c5b2107cb88c24e4f326e6e7a0d5a23
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-10-04 10:55:52 +0200

    Remove unused code

--- src/main/java/life/qbic/util/StringUtil.java
+++ src/main/java/life/qbic/util/StringUtil.java
@@ -2,11 +2,8 @@ package life.qbic.util;
 
 public class StringUtil {
 
-  private StringUtil() {
-  }
-
-  public static boolean endsWithIgnoreCase(String str, String suffix) {
+  public static boolean endsWithIgnoreCase(String input, String suffix) {
     int suffixLength = suffix.length();
-    return str.regionMatches(true, str.length() - suffixLength, suffix, 0, suffixLength);
+    return input.regionMatches(true, input.length() - suffixLength, suffix, 0, suffixLength);
   }
 }
