b81bb2e8e67d204e3ad537ff37dbeceff151c26c
Tobias Koch <kochtobi@users.noreply.github.com>
2023-07-03 16:13:45 +0200

    Provide default value for application and datastore server

--- src/main/java/life/qbic/qpostman/common/options/ServerOptions.java
+++ src/main/java/life/qbic/qpostman/common/options/ServerOptions.java
@@ -9,35 +9,39 @@ import java.util.StringJoiner;
 public class ServerOptions {
 
     @Option(names = {"--application-server"},
-            paramLabel = "url",
-            description = "set the application server to find samples and datasets",
-            hidden = true)
-    public String as_url;
+        paramLabel = "url",
+        description = "set the application server to find samples and datasets",
+        hidden = true)
+    public String as_url = "https://qbis.qbic.uni-tuebingen.de/openbis/openbis";
 
     @Option(names = {"--datastore-server"},
-            paramLabel = "url",
-            description = "add a data store server to find files on",
-            hidden = true)
-    public List<String> dss_urls = new ArrayList<>();
+        paramLabel = "url",
+        description = "add a data store server to find files on",
+        hidden = true)
+    public List<String> dss_urls = new ArrayList<>(List.of(
+        "https://qbis.qbic.uni-tuebingen.de/datastore_server",
+        "https://qbis.qbic.uni-tuebingen.de/datastore_server2"
+    ));
+
 
     @Option(names = {"--source-sample-type"},
-            paramLabel = "Q_SAMPLE_TYPE",
-            description = "the sample type in openBis considered to be the source sample",
-            hidden = true)
+        paramLabel = "Q_SAMPLE_TYPE",
+        description = "the sample type in openBis considered to be the source sample",
+        hidden = true)
     public String sourceSampleType = "Q_TEST_SAMPLE";
 
     @Option(names = {"--server-timeout"},
-            paramLabel = "milliseconds",
-            description = "the server timeout in milliseconds",
-            hidden = true)
+        paramLabel = "milliseconds",
+        description = "the server timeout in milliseconds",
+        hidden = true)
     public long timeoutInMillis = 10_000;
 
     @Override
     public String toString() {
 
         return new StringJoiner(", ", ServerOptions.class.getSimpleName() + "[", "]")
-                .add("as_url='" + as_url + "'")
-                .add("dss_urls=" + dss_urls)
-                .toString();
+            .add("as_url='" + as_url + "'")
+            .add("dss_urls=" + dss_urls)
+            .toString();
     }
 }
