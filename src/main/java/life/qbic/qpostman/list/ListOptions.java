9eb62d15c0593180f371780d0a89f0ecf02b9434
Tobias Koch <kochtobi@users.noreply.github.com>
2023-07-11 13:41:35 +0200

    Add --format option

--- src/main/java/life/qbic/qpostman/list/ListOptions.java
+++ src/main/java/life/qbic/qpostman/list/ListOptions.java
@@ -6,24 +6,32 @@ import picocli.CommandLine.Option;
 
 public class ListOptions {
     @Option(names = "--with-checksum", defaultValue = "false",
-            description = "list the crc32 checksum for each file")
+        description = "list the crc32 checksum for each file")
     public boolean withChecksum;
 
     @Option(names = "--exact-filesize", defaultValue = "false",
-            description = "use exact byte count instead of unit suffixes: Byte, Kilobyte, Megabyte, Gigabyte, Terabyte and Petabyte",
-            showDefaultValue = Visibility.ON_DEMAND)
+        description = "use exact byte count instead of unit suffixes: Byte, Kilobyte, Megabyte, Gigabyte, Terabyte and Petabyte",
+        showDefaultValue = Visibility.ON_DEMAND)
     public boolean exactFilesize;
 
+    @Option(names = "--format", defaultValue = "LEGACY",
+        converter = OutputFormat.OutputFormatConverter.class,
+        completionCandidates = OutputFormat.CompletionCandidates.class,
+        description = "the format to list files in. Possible values: ${COMPLETION-CANDIDATES}",
+        showDefaultValue = Visibility.ALWAYS,
+        required = true)
+    public OutputFormat outputFormat;
+
     @Option(names = "--without-header", defaultValue = "false",
-        description = "remove the header line from the output",
+        description = "remove the header line from the output. Only takes effect for tabular output formats.",
         showDefaultValue = Visibility.ON_DEMAND)
     public boolean withoutHeader;
 
     @Override
-    public String toString() {
+    public String toString(){
         return new StringJoiner(", ", ListOptions.class.getSimpleName() + "[", "]")
-                .add("withChecksum=" + withChecksum)
-                .add("exactFilesize=" + exactFilesize)
-                .toString();
+            .add("withChecksum=" + withChecksum)
+            .add("exactFilesize=" + exactFilesize)
+            .toString();
     }
 }
