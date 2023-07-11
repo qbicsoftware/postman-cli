370c631523634fa901a30bdf9803fd7d9c3403fa
Tobias Koch <kochtobi@users.noreply.github.com>
2023-07-11 13:44:11 +0200

    implement option flag for format

--- src/main/java/life/qbic/qpostman/list/ListCommand.java
+++ src/main/java/life/qbic/qpostman/list/ListCommand.java
@@ -8,9 +8,12 @@ import ch.ethz.sis.openbis.generic.dssapi.v3.IDataStoreServerApi;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import life.qbic.qpostman.common.AuthenticationException;
-import life.qbic.qpostman.common.FileSizeFormatter;
 import life.qbic.qpostman.common.functions.FileFilter;
 import life.qbic.qpostman.common.functions.FindSourceSample;
 import life.qbic.qpostman.common.functions.SearchDataSets;
@@ -22,7 +25,7 @@ import life.qbic.qpostman.common.options.SampleIdentifierOptions;
 import life.qbic.qpostman.common.options.ServerOptions;
 import life.qbic.qpostman.common.structures.DataFile;
 import life.qbic.qpostman.common.structures.DataSetWrapper;
-import life.qbic.qpostman.common.structures.FileSize;
+import life.qbic.qpostman.list.LegacyOutputFormatter.DataSetSummary;
 import life.qbic.qpostman.openbis.ConnectionException;
 import life.qbic.qpostman.openbis.OpenBisSessionProvider;
 import life.qbic.qpostman.openbis.ServerFactory;
@@ -61,15 +64,12 @@ public class ListCommand implements Runnable {
                 .sorted(functions.sortFiles().comparator())
                 .toList();
 
-            boolean withHeader = !listOptions.withoutHeader;
-            String tsvContent = functions.dataFileFormatter()
-                .formatAsTable(processedFiles, "\t", withHeader);
-            long totalBytes = processedFiles.stream()
-                .map(DataFile::fileSize)
-                .mapToLong(FileSize::bytes)
-                .sum();
-            log.info("Output: " + processedFiles.size() + " files (" + FileSizeFormatter.format(
-                FileSize.of(totalBytes), 6) + ")\n" + tsvContent);
+            Consumer<List<DataFile>> output = switch (listOptions.outputFormat) {
+                case LEGACY -> this::listAsLegacy;
+                case TSV -> this::listAsTsv;
+            };
+            output.accept(processedFiles);
+
         } catch (RemoteAccessException remoteAccessException) {
             log.error(
                 "Failed to connect to OpenBis: " + remoteAccessException.getCause().getMessage());
@@ -93,6 +93,26 @@ public class ListCommand implements Runnable {
         }
     }
 
+    private void listAsTsv(List<DataFile> processedFiles) {
+        boolean withHeader = !listOptions.withoutHeader;
+        DataFileTableFormatter dataFileTableFormatter = new DataFileTableFormatter(listOptions.exactFilesize);
+        String tsvContent = dataFileTableFormatter
+            .formatAsTable(processedFiles, "\t", withHeader);
+        System.out.println(tsvContent);
+    }
+
+    private void listAsLegacy(List<DataFile> processedFiles) {
+        LegacyOutputFormatter legacyOutputFormatter = new LegacyOutputFormatter();
+        Map<DataSetWrapper, List<DataFile>> groupedFiles = processedFiles.stream()
+            .collect(Collectors.groupingBy(DataFile::dataSet));
+        for (Entry<DataSetWrapper, List<DataFile>> dataSetWrapperListEntry : groupedFiles.entrySet()) {
+            String output = legacyOutputFormatter.format(
+                new DataSetSummary(dataSetWrapperListEntry.getValue()),
+                listOptions.exactFilesize);
+            System.out.println(output);
+        }
+    }
+
     private Functions setupFunctions() {
         IApplicationServerApi applicationServerApi = ServerFactory.applicationServer(
             serverOptions.as_url, serverOptions.timeoutInMillis);
@@ -105,15 +125,14 @@ public class ListCommand implements Runnable {
             .withSuffixes(filterOptions.suffixes);
         SearchFiles searchFiles = new SearchFiles(dataStoreServerApis, number -> {});
         FindSourceSample findSourceSample = new FindSourceSample(serverOptions.sourceSampleType);
-        DataFileFormatter dataFileFormatter = new DataFileFormatter(listOptions.exactFilesize);
+
         SortFiles sortFiles = new SortFiles();
 
         DataSetWrapper.setFindSourceFunction(findSourceSample);
-      return new Functions(searchDataSets, myAwesomeFileFilter, searchFiles,
-            dataFileFormatter, sortFiles);
+      return new Functions(searchDataSets, myAwesomeFileFilter, searchFiles, sortFiles);
     }
 
-    private record Functions(SearchDataSets searchDataSets, FileFilter fileFilter, SearchFiles searchFiles, DataFileFormatter dataFileFormatter, SortFiles sortFiles) {
+    private record Functions(SearchDataSets searchDataSets, FileFilter fileFilter, SearchFiles searchFiles, SortFiles sortFiles) {
 
     }
 }
